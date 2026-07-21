a8223650bd1635efdda22b26338eac00222345ab
AishwaryaLole <loleaishwarya29@gmail.com>
2026-07-21 19:01:26 +0530

    Added common project templete structure

--- OnlineCabBooking_Backend/src/main/java/com/cabbooking/dto/ApiResponse.java
+++ OnlineCabBooking_Backend/src/main/java/com/cabbooking/dto/ApiResponse.java
@@ -1,17 +1,29 @@
+
 package com.cabbooking.dto;
 
-import lombok.AllArgsConstructor;
+import java.time.LocalDateTime;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class ApiResponse<T> {
+
+    private int statusCode;
 
     private boolean success;
 
     private String message;
 
     private T data;
+
+    private LocalDateTime timestamp = LocalDateTime.now();
+
+    public ApiResponse(int statusCode, boolean success, String message, T data) {
+        this.statusCode = statusCode;
+        this.success = success;
+        this.message = message;
+        this.data = data;
+    }
 }
