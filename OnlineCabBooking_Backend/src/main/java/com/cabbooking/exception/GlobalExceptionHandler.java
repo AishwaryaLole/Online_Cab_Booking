f2dbdf3d5452b4a3b3dcbb81464ff861270f6e37
AishwaryaLole <loleaishwarya29@gmail.com>
2026-07-21 20:21:08 +0530

    Global Exception

--- OnlineCabBooking_Backend/src/main/java/com/cabbooking/exception/GlobalExceptionHandler.java
+++ OnlineCabBooking_Backend/src/main/java/com/cabbooking/exception/GlobalExceptionHandler.java
@@ -1,7 +1,5 @@
 package com.cabbooking.exception;
 
-
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -16,27 +14,43 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ApiResponse<Object>> handleResourceNotFound(ResourceNotFoundException ex) {
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(new ApiResponse<>(false, ex.getMessage(), null));
+                .body(new ApiResponse<>(
+                        HttpStatus.NOT_FOUND.value(),
+                        false,
+                        ex.getMessage(),
+                        null));
     }
 
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<ApiResponse<Object>> handleBadRequest(BadRequestException ex) {
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(new ApiResponse<>(false, ex.getMessage(), null));
+                .body(new ApiResponse<>(
+                        HttpStatus.BAD_REQUEST.value(),
+                        false,
+                        ex.getMessage(),
+                        null));
     }
 
     @ExceptionHandler(UnauthorizedException.class)
     public ResponseEntity<ApiResponse<Object>> handleUnauthorized(UnauthorizedException ex) {
 
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                .body(new ApiResponse<>(false, ex.getMessage(), null));
+                .body(new ApiResponse<>(
+                        HttpStatus.UNAUTHORIZED.value(),
+                        false,
+                        ex.getMessage(),
+                        null));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<Object>> handleException(Exception ex) {
 
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new ApiResponse<>(false, ex.getMessage(), null));
+                .body(new ApiResponse<>(
+                        HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                        false,
+                        "Something went wrong. Please try again later.",
+                        null));
     }
 }
