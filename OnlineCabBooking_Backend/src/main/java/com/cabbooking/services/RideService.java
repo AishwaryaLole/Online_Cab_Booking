971453363ac6fa567039ecf8f90512e4a8645eea
Unnati Warule <unnatiwarule@gmail.com>
2026-07-22 23:38:07 +0530

    added bookRide & getRideById

--- OnlineCabBooking_Backend/src/main/java/com/cabbooking/services/RideService.java
+++ OnlineCabBooking_Backend/src/main/java/com/cabbooking/services/RideService.java
@@ -1,5 +1,33 @@
 package com.cabbooking.services;
 
-public class RideService {
+
+
+import java.util.List;
+
+import com.cabbooking.dto.RideRequestDTO;
+import com.cabbooking.dto.RideResponseDTO;
+
+
+public interface RideService {
+	
+	//Book a new Ride
+	RideResponseDTO bookRide(RideRequestDTO request);
+	
+	//Get rides details by ride ID
+	RideResponseDTO getRideById(Long id);
+	
+	//Get all rides of a passenger
+	List<RideResponseDTO> getRideHistory(Long passengerId);
+	
+	//Cancel a booked ride
+	RideResponseDTO cancelRide(Long id);
+	
+	//Start the ride
+	RideResponseDTO startRide(Long id);
+	
+	//Complete the ride
+	RideResponseDTO completeRide(Long id);
+	
+	
 
 }
