a054fb1d4ef5279aae911622a7261021af258041
AishwaryaLole <147826387+AishwaryaLole@users.noreply.github.com>
2026-07-31 23:47:26 +0530

    admin and login main page
    * Added common project templete structure
    
    * Added common project template structure
    
    * Global Exception
    
    * Simple User Registration completed
    
    * Completed authentication module with JWT, OTP and email verification
    
    * Initial React frontend common template
    
    * Frontend of login page register page and main home page.
    
    * some Modifie auth
    
    * Admin and login

--- online-cab-booking-frontend/src/main.jsx
+++ online-cab-booking-frontend/src/main.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
 
 import App from "./App";
 
@@ -12,14 +10,6 @@ import "leaflet/dist/leaflet.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-
-      <ToastContainer
-        position="top-right"
-        autoClose={3000}
-        theme="light"
-      />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
